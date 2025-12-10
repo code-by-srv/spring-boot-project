7f66d3d356d2d6b56a9ab1586cddadc2d2088e8a
code-by-srv <srvsharma09@gmail.com>
2025-12-11 00:39:55 +0530

    @PostContruct and @PreDestroy annotation added

--- src/main/java/com/srvcode/model1project/PaymentService.java
+++ src/main/java/com/srvcode/model1project/PaymentService.java
@@ -1,10 +1,22 @@
 package com.srvcode.model1project;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentService {
     public void pay(){
         System.out.println("paying.....");
+    }
+
+    @PostConstruct    //Immediately after all beans are initialized
+    public void afterInitialize(){
+        System.out.println("Before payment");
+    }
+
+    @PreDestroy      //Just before all the beans are destroyed
+    public void beforeDestroy(){
+        System.out.println("After payment is done");
     }
 }
